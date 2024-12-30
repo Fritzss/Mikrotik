2f5fe11a50f1120dc56edae5dd033a157d5c3c7f
Fritzss <Sergey.Nik.Krylov@gmail.com>
2024-12-30 11:03:55 +0300

    Update createTableRosLogs.sql

--- createTableRosLogs.sql
+++ createTableRosLogs.sql
@@ -1,11 +1,15 @@
 CREATE DATABASE logs; 
-CREATE TABLE logs.ros_logs
+CREATE TABLE IF NOT EXISTS logs.ros
 (
     `host` String,
     `message` String,
-    `source_app` String,
+    `source_type` String,
+    `Topics` String,
+    `port` UInt16,
     `timestamp` DateTime
 )
 ENGINE = MergeTree
+PARTITION BY Topics
 ORDER BY timestamp
-SETTINGS index_granularity = 8192 
+TTL timestamp + toIntervalMonth(12)
+SETTINGS index_granularity = 8192
