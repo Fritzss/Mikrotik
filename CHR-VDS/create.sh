7df704f8e589bbeffe148c9ab8623d86c7c6cbca
Fritzss <Sergey.Nik.Krylov@gmail.com>
2025-10-03 17:13:20 +0300

    Update create.sh

--- CHR-VDS/create.sh
+++ CHR-VDS/create.sh
@@ -7,7 +7,7 @@ unzip chr-$VERSION.img.zip
 DISK=$(fdisk -l | grep -i "disk /dev/" | awk -F ' ' '{print $2}')
 DISK="${DISK: :-1}
 
-dd if= chr-$VERSION.img of=$DISK bs=4M oflag=sync
+dd if=chr-$VERSION.img of=$DISK bs=4M oflag=sync
 
 echo 1 > /proc/sys/kernel/sysrq
 echo b > /proc/sysrq-trigger
