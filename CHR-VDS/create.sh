91f79ce78bb94ba12e0a914604556e2ddeb100c9
Fritzss <Sergey.Nik.Krylov@gmail.com>
2025-10-07 09:24:45 +0300

    Update create.sh

--- CHR-VDS/create.sh
+++ CHR-VDS/create.sh
@@ -1,4 +1,6 @@
-VERSION="7.20" 
+#!/bin/bash
+
+VERSION="7.20"
 USER="<user_name>"
 PASSWORD="<super_password>"
 
@@ -8,12 +10,12 @@ wget https://download.mikrotik.com/routeros/$VERSION/chr-$VERSION.img.zip
 sudo apt-get install unzip
 unzip chr-$VERSION.img.zip
 DISK=$(fdisk -l | grep -i "disk /dev/" | awk -F ' ' '{print $2}')
-DISK="${DISK: :-1}
+DISK="${DISK: :-1}"
 sudo mkdir -p /mnt/chr
 
 SS=$(fdisk -lu chr-7.20.img | grep -i "sector size" | awk -F ' ' '{print $4}')
 FS=$(fdisk -lu chr-7.20.img | grep -i "img2" | awk -F ' ' '{print $2}')
-OFFSET=$(( $$SS * $FS ))
+OFFSET=$(( $SS * $FS ))
 
 sudo mount -o loop,offset=$OFFSET chr-7.20.img /mnt/chr
 
@@ -29,3 +31,4 @@ sudo dd if=chr-$VERSION.img of=$DISK bs=4M oflag=sync
 
 sudo echo 1 > /proc/sys/kernel/sysrq
 sudo echo b > /proc/sysrq-trigger
+
